ccedec1adf1143e643820aa70f7ee0b91f7a70fc
hyuckkim <monkeyhyuck@gmail.com>
2024-03-08 11:48:16 +0900

    post/id의 로딩을 load 함수로 이동

--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -1,46 +1,17 @@
 <script lang="ts">
-    import { _id } from "./+page";
-    import { _getData } from "$lib";
-    import { onMount } from "svelte";
+    import type { PageData } from "./$types";
 
-    import { unified } from "unified";
-    import markdown from "remark-parse";
-    import remarkRehype from "remark-rehype";
-    import html from "rehype-stringify";
-    import rehypeHighlight from "rehype-highlight";
-
-    let data: {
-        metadata: {
-            [key: string]: string;
-        };
-        data: string;
-    } | null = null;
-
-    let content = "";
-
-    onMount(async () => {
-        data = await _getData(_id);
-        content = (await unified()
-            .use(markdown)
-            .use(remarkRehype)
-            .use(html)
-            .use(rehypeHighlight)
-            .process(data.data))
-            .toString();
-    });
+    export let data: PageData;
 </script>
 
-{#if !!data}
 <section class="top">
-    <h1>{data.metadata.title}</h1>
-    <p>{data.metadata.date}</p>
+    <h1>{data.title}</h1>
+    <p>{data.date}</p>
 </section>
 <section>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark-reasonable.min.css">
-    <div bind:innerHTML={content} contenteditable="false" />
+    <div bind:innerHTML={data.content} contenteditable="false" />
 </section>
-{/if}
-
 <style>
     .top {
         display: flex;
--- src/routes/post/[id]/+page.ts
+++ src/routes/post/[id]/+page.ts
@@ -1,8 +1,25 @@
 import type { PageLoad } from "./$types"
 
-export let _id = "";
-export const load: PageLoad = ({ params }) => {
-    if (typeof params.id === "string") {
-        _id = params.id;
+import { unified } from "unified";
+import markdown from "remark-parse";
+import remarkRehype from "remark-rehype";
+import html from "rehype-stringify";
+import rehypeHighlight from "rehype-highlight";
+import { _getData } from "$lib";
+
+export const load: PageLoad = async ({ params }) => {
+    const data = await _getData(params.id);
+
+    return {
+        title: data.metadata.title,
+        date: data.metadata.date,
+        tags: data.metadata.tags.split(', '),
+        content: (await unified()
+        .use(markdown)
+        .use(remarkRehype)
+        .use(html)
+        .use(rehypeHighlight)
+        .process(data.data))
+        .toString()
     }
 }
