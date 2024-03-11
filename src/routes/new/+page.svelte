f42d19dc02bb200721cdfbce6fcd59d2225238f8
hyuckkim <monkeyhyuck@gmail.com>
2024-03-11 14:42:01 +0900

    에디터에서 몇몇 단축키 사용

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -6,6 +6,7 @@
     let title = "";
     let tags = "";
     let post = "";
+    let area: HTMLTextAreaElement;
 
     const getDay = () => {
         const today = new Date();
@@ -29,6 +30,68 @@
     const doSubmit = () => {
         navigator.clipboard.writeText(getStr());
     }
+
+    const handleShortcut = (e: KeyboardEvent) => {
+        const keyInput = (key: string, attr: ("ctrl" | "shift" |"alt")[] = [], ev: KeyboardEvent = e) => {
+            if (attr.includes("ctrl") && !ev.ctrlKey) return false;
+            if (attr.includes("shift") && !ev.shiftKey) return false;
+            if (attr.includes("alt") && !ev.altKey) return false;
+
+            if (e.key !== key) return false;
+            return true;
+        }
+
+        if (keyInput("Tab")) {
+            const tabText = "    ";
+            const ln = (idx: number) => (post.slice(0, idx).match(/\n/g) || []).length;
+            const startPos = area.selectionStart, endPos = area.selectionEnd;
+            const startLn = ln(startPos), endLn = ln(endPos);
+            e.preventDefault();
+            if (e.shiftKey) {
+                const firsthasTab = post.split('\n')[startLn].startsWith(tabText);
+                const hasTabLength = post.split('\n').filter((l, i) => (i >= startLn && i <= endLn && l.startsWith(tabText))).length;
+                post = post.split('\n').map((l, i) => (i >= startLn && i <= endLn && l.startsWith(tabText)) ? l.slice(tabText.length) : l).join('\n');
+                setTimeout(() => {
+                    area.setSelectionRange(startPos - (firsthasTab ? tabText.length : 0), endPos - hasTabLength * (endLn - startLn + 1));
+                }, 0);
+            }
+            else {
+                if (startLn === endLn) {
+                    post = [...post.slice(0, startPos), tabText, ...post.slice(endPos)].join('');
+                    setTimeout(() => {
+                        area.setSelectionRange(startPos + tabText.length, startPos + tabText.length);
+                    }, 0);
+                }
+                else {
+                    post = post.split('\n').map((l, i) => (i >= startLn && i <= endLn) ? tabText + l : l).join('\n');
+                    setTimeout(() => {
+                        area.setSelectionRange(startPos + tabText.length, endPos + tabText.length * (endLn - startLn + 1));
+                    }, 0);
+                }
+            }
+        }
+        if (keyInput("s", ["ctrl"])) {
+            doSubmit();
+            e.preventDefault();
+        }
+
+        if (keyInput(".", ["ctrl"])) {
+            isPreview = true;
+
+            setTimeout(() => {
+                const event = (e: KeyboardEvent) => {
+                    if (keyInput(".", ["ctrl"], e)) {
+                        isPreview = false;
+                        window.removeEventListener("keydown", event);
+                        setTimeout(() => {
+                            if (area) area.focus();
+                        }, 0);
+                    }
+                };
+                window.addEventListener("keydown", event);
+            }, 0);
+        }
+    }
 </script>
 
 <small>(뭔가 시도하려다 포기한 흔적)</small>
@@ -45,7 +108,7 @@
 {#if isPreview}
 <Markdown data={post} />
 {:else}
-<textarea bind:value={post} />
+<textarea bind:this={area} bind:value={post} on:keydown={handleShortcut} />
 {/if}
 
 <style>
