875197581e521e8a682ad28dcbb2db3213b1f5c7
hyuckkim <monkeyhyuck@gmail.com>
2024-03-14 00:05:05 +0900

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
             pages: 'build',
             assets: 'build',
             fallback: undefined,
-            precompress: true,
+            precompress: false,
             strict: true
         })
     }
